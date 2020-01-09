ee59a660a161c5c5b46190e34da6042fc67331c1
bruce <805503895@qq.com>
2020-01-09 23:51:34 +0800

    新增冒泡排序

---
+++ src/main/java/com/it/bruce/leetcode/bubbleSort/Main.java
@@ -0,0 +1,39 @@
+package com.it.bruce.leetcode.bubbleSort;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] arr = new int[10000];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Math.toIntExact(Math.round(Math.random() * 1000));
+        }
+        bubbleSortII(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+
+    private static void bubbleSortII(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = arr.length - 1; j > i; j--) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+}
